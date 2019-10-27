80e8131c832a328461331556611780a71afa2d9d
Sean Leif Hellier <sean@newtropolis.com>
2019-10-27 12:21:21 -0400

    working except for moving burgers

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Impossible Burger', 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Beyond Burger', 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Fish Burger', 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Martian Burger', 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Celery Burger', 0);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Strawberry Burger', 0);
